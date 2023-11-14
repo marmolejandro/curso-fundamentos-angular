d7af26d757ccb3c3495b7ad8f54b7f672c6c181e
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-14 09:14:29 -0500

    Se hacen ejemplos de curso, para mostrar el uso de: -typeScript - Manejo de Clases - String Interpolation - Property Binding - Event Binding

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -10,5 +10,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Alejandro';
+  age = 18;
+  img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    name: 'Alejandro',
+    age: 17,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
---
+++ my-store/src/app/recapt.ts
@@ -0,0 +1,14 @@
+const username: string | number = 'marmolejandro';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+sum(1, 2)
+
+class Person {
+
+  constructor(private age: number, public lastName: string){}
+}
+
+const nico = new Person(15, 'Molina');
